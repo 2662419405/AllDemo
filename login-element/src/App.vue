d8929be463d5498688a4d974aa361dced5becc83
sh <2662419405@qq.com>
2019-12-29 08:23:38 +0800

    :fire:初步搭建完element表单

--- login-element/src/App.vue
+++ login-element/src/App.vue
@@ -1,32 +1,56 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>Element表单</h3>
+      <hr>
+      <el-form :model="model" :rules="rules" ref="loginForm">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="model.username" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="确认密码" prop="password">
+          <el-input type="password" v-model="model.password" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('loginForm')">提交</el-button>
+        </el-form-item>
+      </el-form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name:'app1',
+  data() {
+      return {
+        value:'',
+        model: { username: "tom", password: "" },
+        rules: {
+          username: [
+            { required: true, message: "请输入用户名" },
+            {min: 6,max:10,message:'请输入6~10的用户名'}
+          ],
+          password: [
+            { required: true, message: "请输入密码" }
+          ],
+        }
+      };
+    },
+    methods: {
+        submitForm(form) {
+            this.$refs[form].validate(valid=>{
+                if (valid) {
+                    alert('请求登录!')
+                } else {
+                    alert('校验失败！')
+                }
+            })
+        }
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- login-element/src/plugins/element.js
+++ login-element/src/plugins/element.js
@@ -1,4 +1,7 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button,Form,FormItem,Input } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
