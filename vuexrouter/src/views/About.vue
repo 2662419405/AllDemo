0b0971bd2a1a85c0918168301f21a8a650220a1f
sh <2662419405@qq.com>
2019-12-29 21:30:51 +0800

    测试axios

--- vuexrouter/src/views/About.vue
+++ vuexrouter/src/views/About.vue
@@ -3,3 +3,13 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+import axios from 'axios'
+export default {
+  created() {
+    axios.get('http://shtodream.cn:7300/mock/5e08a4657d30fb4da2043fcb/demo/user').then((res)=>{
+      console.log(res)
+    })
+  },
+}
+</script>
