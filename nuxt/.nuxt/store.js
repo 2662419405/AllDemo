7779a8f2ea588a3c1b45166cc3024228ddb0c358
sh <2662419405@qq.com>
2020-01-03 20:18:08 +0800

    build成功

--- nuxt/.nuxt/store.js
+++ nuxt/.nuxt/store.js
@@ -12,29 +12,12 @@ let store = {};
 
   // If store is an exported method = classic mode (deprecated)
 
-  if (typeof store === 'function') {
-    return console.warn('Classic mode for store/ is deprecated and will be removed in Nuxt 3.')
-  }
-
   // Enforce store modules
   store.modules = store.modules || {}
 
   resolveStoreModules(require('..\\store\\users.js'), 'users.js')
 
   // If the environment supports hot reloading...
-
-  if (process.client && module.hot) {
-    // Whenever any Vuex module is updated...
-    module.hot.accept([
-      '..\\store\\index.js',
-      '..\\store\\users.js',
-    ], () => {
-      // Update `root.modules` with the latest definitions.
-      updateModules()
-      // Trigger a hot update in the store.
-      window.$nuxt.$store.hotUpdate(store)
-    })
-  }
 })()
 
 // createStore
