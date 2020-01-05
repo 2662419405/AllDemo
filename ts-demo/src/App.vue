dfc6c4830f5ccaae85b248b750198be712af1396
sh <2662419405@qq.com>
2020-01-05 19:11:33 +0800

    测试jest

--- ts-demo/src/App.vue
+++ ts-demo/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <demo />
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -7,11 +8,13 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Demo from './components/Demo.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Demo
   }
 }
 </script>
--- ts-demo/tests/unit/demo.spec.js
+++ ts-demo/tests/unit/demo.spec.js
@@ -1,3 +1,6 @@
+import Vue from 'vue'
+import Demo from '../../src/components/Demo.vue'
+
 function add(num1,num2){
     return num1 + num2
 }
@@ -7,8 +10,20 @@ describe('sunhang',()=>{
     // 测试选项
     it('测试add函数',()=>{
         // 断言
-        expect((add(1,2)).toBe(3))
-        expect((add(1,3)).toBe(4))
-        expect((add(-2,3)).toBe(1))
+        expect(add(1,3)).toBe(4)
+        expect(add(-2,3)).toBe(1)
+    })
+})
+
+// 导入vue进行测试
+describe('Demo',()=>{
+    // 检测生命周期
+    it('检测生命周期',()=>{
+        expect(typeof Demo.created ).toBe('function')
+    })
+    // 检测data是数据是什么
+    it('初始化data是vue-text', ()=>{
+        expect(typeof Demo.data).toBe('function')
+        expect(Demo.data().message).toBe('vue-text')
     })
 })
