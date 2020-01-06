3f89dcd2e5014568e635d60479f78dea94ba025d
sh <2662419405@qq.com>
2020-01-06 14:40:43 +0800

    :rainbow: react组件化

---
+++ react-antd/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  Button
+} from 'antd/dist/antd';
+import "antd/dist/antd.css"
+
+function App() {
+  return (
+    <div className="App">
+      123
+      <Button>
+        按钮
+      </Button>
+    </div>
+  );
+}
+
+export default App;
