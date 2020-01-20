e3a47d1a5211a134541f1a0f18774329e8c29ba9
sh <2662419405@qq.com>
2020-01-20 23:07:13 +0800

    :tada: 修改了webhooks

---
+++ node部署/taro-docker/deploy.sh
@@ -0,0 +1,6 @@
+# 进入目录
+cd /usr/local/nginx/html/
+rm -rf sh
+# 获取新文件
+git clone git@github.com:2662419405/2662419405.github.io.git
+mv 2662419405.github.io sh
--- node部署/taro-docker/webhooks.js
+++ node部署/taro-docker/webhooks.js
@@ -1,6 +1,6 @@
 var http = require('http')
 var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/webhooks', secret: 'myHashSecret' })
+var handler = createHandler({ path: '/webhooks', secret: '2662419405@qq' })
 // 上面的 secret 保持和 GitHub 后台设置的一致
 
 function run_cmd(cmd, args, callback) {
@@ -11,8 +11,6 @@ function run_cmd(cmd, args, callback) {
     child.stdout.on('data', function (buffer) { resp += buffer.toString(); });
     child.stdout.on('end', function () { callback(resp) });
 }
-// debug用
-// run_cmd('sh', ['./deploy-dev.sh'], function(text){ console.log(text) });
 
 http.createServer(function (req, res) {
 
@@ -20,19 +18,9 @@ http.createServer(function (req, res) {
         res.statusCode = 404
         res.end('no such location')
     })
-}).listen(7777,() =>{
-    console.log('WebHooks Listern at 7777');
+}).listen(5000,() =>{
+    console.log('WebHooks Listern at 5000');
 })
-
-handler.on('error', function (err) {
-    console.error('Error:', err.message)
-})
-
-
-// handler.on('*', function (event) {
-//     console.log('Received *', event.payload.action);
-//     //   run_cmd('sh', ['./deploy-dev.sh'], function(text){ console.log(text) });
-// })
 
 handler.on('push', function (event) {
     console.log('Received a push event for %s to %s',
@@ -41,17 +29,7 @@ handler.on('push', function (event) {
         // 分支判断
         if(event.payload.ref === 'refs/heads/master'){
             console.log('deploy master..')
-            run_cmd('sh', ['./deploy-dev.sh'], function(text){ console.log(text) });
-
+            run_cmd('sh', ['./deploy.sh'], function(text){ console.log(text) });
         }
 })
-
-
-// handler.on('issues', function (event) {
-//     console.log('Received an issue event for % action=%s: #%d %s',
-//         event.payload.repository.name,
-//         event.payload.action,
-//         event.payload.issue.number,
-//         event.payload.issue.title)
-// })
 
