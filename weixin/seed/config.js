33395b34764f6633f531eb05baeb2ebb4182cea5
sh <2662419405@qq.com>
2020-01-21 19:51:48 +0800

    :tada: 微信公众号测试

---
+++ weixin/seed/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    appid:'wx9ca580bf0cde0b07',
+    appsecret:'8f1539b3a8ea0e973fc010a89894608b',
+    token:'sunhang'
+}
--- weixin/seed/index.js
+++ weixin/seed/index.js
@@ -5,7 +5,16 @@ const bodyParser = require('koa-bodyparser');
 const app = new Koa()
 app.use(bodyParser())
 const router = new Router()
+const conf = require('./config')
 app.use(static(__dirname + '/'))
+
+const wechat = require('co-wechat')
+router.all('/weixin', wechat(conf).middleware(
+    async message => {
+        console.log('wechat:', message)
+        return 'Hello World ' + message.Content
+    }
+))
 
 app.use(router.routes()); /*启动路由*/
 app.use(router.allowedMethods());
