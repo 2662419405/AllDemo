4919e1f4f6d7049d65e548a0f3bd24921a4138b7
sh <2662419405@qq.com>
2020-01-29 13:16:22 +0800

    :apple: 分页完成!

--- yun/miniprogram/pages/index/index.js
+++ yun/miniprogram/pages/index/index.js
@@ -1,88 +1,112 @@
+//index.js
+const app = getApp()
 const db = wx.cloud.database()
-// miniprogram/pages/index/index.js
-Page({
 
-  /**
-   * 页面的初始数据
-   */
+Page({
   data: {
+    userInfo: wx.getStorageSync('userinfo') || {},
     books: [],
     page: 0,
+    more: true,
   },
-  onReachBottom(){
-    console.log('触发底部了')
+  // onsHARE
+  onPullDownRefresh() {
+    console.log('pulldown')
+
+    this.getList(true)
+
+    // console.log('xx')
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  onReachBottom() {
+    console.log("bottom")
+
+    this.setData({
+      page: this.data.page + 1
+    }, () => {
+      console.log(this.data.page)
+      this.getList()
+
+    })
+  },
+
+
+  getList(init) {
+    if (init) {
+      this.setData({
+        page: 0,
+        more: true
+      })
+      // 初始化
+
+    }
+    wx.showNavigationBarLoading()
+    wx.showLoading({ title: '加载中' })
+    const PAGE = 3
+    const offset = this.data.page * PAGE
+    console.log(offset)
+    let ret = db.collection('doubanbooks').orderBy('create_time', 'desc')
+    if (offset > 0) {
+      ret = ret.skip(offset)
+    }
+
+    ret = ret.limit(PAGE).get().then(books => {
+
+      if (books.data.length < Page && this.data.page > 0) {
+        this.setData({
+          more: false
+        })
+      }
+      // books.data.length = PAGE
+      if (init) {
+        this.setData({
+          books: books.data
+        })
+        wx.stopPullDownRefresh()
+      } else {
+        this.setData({
+          books: [...this.data.books, ...books.data]
+        })
+        // 下拉刷新，不能直接覆盖books 而是累加
+      }
+      wx.hideLoading()
+      wx.hideNavigationBarLoading()
+    })
+    // const offset = 
+
+
+
+
+    //  .get({
+    // success:(books)=>{
+    //   if (books.data.length < Page && this.data.page > 0) {
+    //     console.log(books.data)
+    //     this.setData({
+    //       more:false
+    //     })
+    //   }
+    //   // books.data.length = PAGE
+    //   if (init) {
+    //     this.setData({
+    //       books: books.data
+    //     })
+    //     wx.stopPullDownRefresh()
+    //   } else {
+    //     this.setData({
+    //       books: [...this.data.books, ...books.data]
+    //     })
+    //     // 下拉刷新，不能直接覆盖books 而是累加
+    //   }
+    //   wx.hideLoading()
+    //   wx.hideNavigationBarLoading()
+    // }
+    // }).then(ret=>{
+    //   console.log(ret)
+    // })
+
+  },
+  onLoad: function () {
     this.getList(true)
   },
-  getList(){
-    const PAGE = 3
-    const offest = this.data.page * PAGE
-    let ret = db.collection('doubanbooks').orderBy('create_time', 'desc')
-    if(this.data.page>1){
-      ret = ret.skip(offest)
-    }
-    ret = ret.limit(PAGE).get().then(res=>{
-      this.setData({
-        books: res.data
-      })
-    })
-    // db.collection('doubanbooks').orderBy('create_time', 'desc').get().then(res => {
-    //   this.setData({
-    //     books: res.data
-    //   })
-    // })
-  },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
 
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
