0e1aab16d74d065231cf3c64a02740374906a38d
孙航 <2662419405@qq.com>
2020-06-04 16:50:50 +0800

    fect: 修复和完善localStorage功能

--- base/localStorge/index.js
+++ base/localStorge/index.js
@@ -8,20 +8,27 @@ const moment = require('moment')
 class Storage {
   constructor() {
     this.storage = window.localStorage
-    this.time = 3600 * 60 * 24
   }
   getItem(key) {
-    const origin = Base64.decode(this.storage.getItem(key))
-    
+    const origin = JSON.parse(Base64.decode(this.storage.getItem(key)))
+    const { time } = origin
+    if (time > new Date.getItem()) {
+      return {
+        ...origin,
+        message: `'过期时间', ${moment(time).format('YYYY-MM-DD HH:mm:ss')}`,
+      }
+    } else {
+      this.removeItem(key)
+      return ''
+    }
   }
-  setItem(key, value) {
-    this.storage.setItem(key, Base64.encode(value)) // 简单加密
+  setItem(key, value, time = new Date.getItem() + 3600 * 24) {
+    if (typeof time === 'object') {
+      value = { value }
+    }
+    this.storage.setItem(key, Base64.encode(JSON.stringify({ ...value, time }))) // 简单加密
   }
   removeItem(key) {
     this.storage.removeItem(key)
   }
 }
-
-const storage = new Storage()
-
-storage.removeItem('name')
